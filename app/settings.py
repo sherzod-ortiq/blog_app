308452a176f93f37fb1a4ce1f0eb527d5707fa3e
software2015 <hhll69241@gmail.com>
2019-12-15 05:00:19 +0300

    Changing the development database and adding a few other features

--- app/settings.py
+++ app/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_app_development',
+        'USER': 'blog_app',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
+
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+
 }
 
 
@@ -120,3 +131,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+LOGIN_REDIRECT_URL = '/'
