f7ba181e063c69b3b6607e79afc53d0b351dedbd
software2015 <hhll69241@gmail.com>
2019-12-19 05:34:11 +0300

    Test server configurations

--- app/settings/test.py
+++ app/settings/test.py
@@ -1,14 +1,16 @@
 from .base import *
 
-#DATABASES = {
-#
-#  'default': {
-#    'ENGINE': 'django.db.backends.mysql',
-#    'NAME': 'Sherzodbek$blog_app_test',
-#    'USER': 'Sherzodbek',
-#    'PASSWORD': 'blog_app_test',
-#    'HOST': 'Sherzodbek.mysql.pythonanywhere-services.com',
-#    'PORT': '',
-#  }
-#
-#}
+"""
+DATABASES = {
+
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'Sherzodbek$blog_app_test',
+    'USER': 'Sherzodbek',
+    'PASSWORD': 'blog_app_test',
+    'HOST': 'Sherzodbek.mysql.pythonanywhere-services.com',
+    'PORT': '',
+  }
+
+}
+"""
