4b7b4c6920694d2d523f601ec0e78738a565232d
software2015 <hhll69241@gmail.com>
2019-12-19 04:40:02 +0300

    Test server configurations

--- app/settings/test.py
+++ app/settings/test.py
@@ -1,1 +1,14 @@
 from .base import *
+
+DATABASES = {
+
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'blog_app_test',
+    'USER': 'blog_app',
+    'PASSWORD': 'password',
+    'HOST': 'localhost',
+    'PORT': '',
+  }
+
+}
